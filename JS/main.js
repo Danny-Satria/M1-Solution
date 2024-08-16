059979283923258a29c9407f3eea586f8c3717d3
Danny-Satria <danny.satriaku@gmail.com>
2024-08-16 16:44:48 +0700

    menyesuaikan posisi logo navbar

--- JS/main.js
+++ JS/main.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function applyMediaQuery() {
   const element = document.querySelector(".outter");
   if (window.innerWidth <= 600) {
-    element.style.left = "-9em";
+    element.style.left = "-17em";
   }
 }
 
